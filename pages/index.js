6992152937d699ecdac1814e87451330debf16f0
vd-mjkhan <muhammad.junaid@venturedive.com>
2019-12-17 10:38:25 +0500

    Created static order register

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
-import { Layout, Page, TextStyle } from '@shopify/polaris';
+import { Card, Layout } from '@shopify/polaris';
 
 const Index = () => (
-  <Page>
-    <Layout>
-      <TextStyle variation="positive">
-        Sample app using React and Next.js
-      </TextStyle>
-    </Layout>
-  </Page>
+  <Layout>
+    <Layout.Section>
+      <Card title="Don't be hot" sectioned>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </Card>
+    </Layout.Section>
+  </Layout>
 );
+
 
 export default Index;
